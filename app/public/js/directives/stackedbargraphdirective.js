69183880fa58e7d33e9bb1e39eeee0472cc6ef56
Hanna Liebl <hanna.liebl@gmail.com>
2014-12-01 23:21:15 -0800

    simplify stacked bar graph, add display options to it

--- app/public/js/directives/stackedbargraphdirective.js
+++ app/public/js/directives/stackedbargraphdirective.js
@@ -4,41 +4,56 @@ nwslData
   return {
     restrict: "AE",
     scope: {
-      source: "@",
-      scalex: "@",
-      scaley: "@",
       svgId: "@",
       labelx: "@",
-      labely: "@",
       team: "&",
       hover: '&',
       hoverLeave: '&',
       sort: '&',
-      year: "=",
       title: "@",
       hoverText: "@",
+      hoverText2: "@",
       sortText: "@",
-      show: "="
+      show: "=",
+      showteams: "@"
     },
     templateUrl: '/js/directives/templates/stackedbargraphtemplate.html',
     link: function (scope, element, attrs) {
-      nwslDataService.offFrameShots("2014").then(function (data) {
+      nwslDataService[attrs.source](attrs.year).then(function (data) {
         var margin = {top: 20, right: 15, bottom: 120, left: 40},
           width = 950 - margin.left - margin.right,
           height = 700 - margin.top - margin.bottom;
 
         var teamBackgrounds = {
           "all": {
-            img: "kimlittle.jpg",
-            credit: "https://www.flickr.com/photos/alza06/3530677425/in/set-72157618155404356"
+            img: "nwsl_fade.jpg"
           },
           "boston": {
-            img: "hao.jpg",
-            credit: "http://www.flickr.com/photos/curoninja/2760299890/in/photolist-5cVfZq-5cQWtP-5cQWqt-5cQWoK-5cQWnn-c9aumG-c9aufQ-c9atVY-c9atTs-c9atQY-c9atGb-c9arH7-c9arD3-c9arj7-c7ows1-bnBDS6-aGRj1R-asmWsK-9LwteA-KR6q"
+            img: "boston_fade.jpg"
           },
           "chicago": {
-            img: "press.jpg",
-            credit: "https://www.flickr.com/photos/hermancaroan/14015891396"
+            img: "chicago_fade.jpg"
+          },
+          "washington dc": {
+            img: "dc_fade.jpg"
+          },
+          "kansas city": {
+            img: "kansascity_fade.jpg"
+          },
+          "portland": {
+            img: "portland_fade.jpg"
+          },
+          "seattle": {
+            img: "seattle_fade.jpg"
+          },
+          "new jersey": {
+            img: "newjersey_fade.jpg"
+          },
+          "western ny": {
+            img: "rochester_fade.jpg"
+          },
+          "houston": {
+            img: "houston_fade.jpg"
           }
         };
 
@@ -87,7 +102,7 @@ nwslData
         var y = d3.scale.linear()
           .range([height, 0]);
 
-        var chart = d3.select("#stacked").append("svg")
+        var chart = d3.select("#"+scope.svgId).append("svg")
           .attr("width", width + margin.left + margin.right)
           .attr("height", height + margin.top + margin.bottom)
           .append("g")
@@ -114,27 +129,19 @@ nwslData
             .attr("y", -30)
             .attr("x", -220)
             .attr("transform", "rotate(-90)")
-            .text(scope.labely);
-
-        var color = d3.scale.ordinal()
-          .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
+            .text(attrs.labely);
 
         function update (data) {
-          color.domain(d3.keys(data[0]).filter(function(key) { return key == "d.NAME"; }));
-
+ 
           data.forEach(function(d) {
             var y0 = 0;
-            //d.allShots = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
-            d.allShots = [{name: "Shots on Goal", y0: 0, y1: d.SOG, team: d.team}, {name: "Off Target Shofts", y0: d.SOG, y1: d.SH, team: d.team}];
-            d.total = d.SH;
+            d.allShots = [{player: d.NAME, name: "Shots on Goal", y0: 0, y1: d.SOG, team: d.team, totalShots: d.SH, shotsOnGoal: d.SOG}, {player: d.NAME, name: "Off Target Shots", y0: d.SOG, y1: d.SH, team: d.team, totalShots: d.SH, shotsOnGoal: d.SOG}];
           });
 
           data.sort(function(a, b) { return b.team - a.team; });
-          console.log(data);
 
-          x.domain(data.map(function(d) { return d.NAME; }));
-          y.domain([0, d3.max(data, function(d) { return d.SH; })]);
+          x.domain(data.map(function(d) { return d[attrs.scalex]; }));
+          y.domain([0, d3.max(data, function(d) { return d[attrs.scaley]; })]);
           
           xAxisG
             .transition()
@@ -160,82 +167,80 @@ nwslData
             .duration(500)
 
           var players = chart.selectAll('.players')
-            .data(data)
-            .enter().append("g")
-            .attr("class", "g")
-            .attr("transform", function(d) { return "translate(" + x(d.NAME) + ",0)"; });
+            .data(data);
+
+          players.transition()
+            .duration(500)
+            .attr("class", "bar")
+            .attr("transform", function(d) { return "translate(" + x(d[attrs.scalex]) + ",0)"; })
+              
+          players.enter()
+            .append("g")
+            .attr("class", "bar")
+            .transition()
+              .duration(500)
+              .attr("transform", function(d) { return "translate(" + x(d[attrs.scalex]) + ",0)"; })
+
+          players.exit()
+              .transition()
+              .duration(500)
+              .style("opacity", 0)
+              .remove();
 
           var bars = players.selectAll("rect")
-            .data(function(d) {return d.allShots; })
-          .enter().append("rect")
+            .data(function(d) {return d.allShots; });
+
+          bars.transition()
+            .duration(500)
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d[scope.scalex]); })
+            .attr("y", function(d) { return y(d[scope.scaley]); })
+            .attr("height", function(d) { return height - y(d[scope.scaley]); })
             .attr("width", x.rangeBand())
-            .attr("y", function(d) {
-              return y(d.y1); })
-            .attr("height", function(d) { return Math.abs(y(d.y0) - y(d.y1)); })
             .attr("fill", function(d) {
               return teamColors[d.team].fill;
+            })
+            .attr("stroke", function(d) {
+              return teamColors[d.team].stroke;
             });
 
-          // bars.selectAll("rect")
-          //   .data(function(d) { return d.shots; })
-          // .enter().append("rect")
-          //   .attr("width", x.rangeBand())
-          //   .attr("y", function(d) { return y(d.y1); })
-          //   .attr("height", function(d) { return y(d.y0) - y(d.y1); })
-          //   .style("fill", function(d) { return color(d.name); });
+          bars.enter()
+            .append("rect")
+            .attr("width", x.rangeBand())
+            .attr("x", 0)
+            .attr("y", y(0))
+            .attr("height", 0)
+            .style("opacity", 0)
+            .on('mouseover', function(d,i) {
+              scope.$apply(scope.hover({item: d}));
+            })
+            .on('mouseleave', function(d,i) {
+              scope.$apply(scope.hoverLeave({item: d}));
+            })
+            .attr("fill", function(d) {
+              if (d.name === "Shots on Goal") {
+                return "rgba(0,0,0,0.5)";
+              } else {
+                return teamColors[d.team].fill;
+              }
+            })
+            .transition()
+              .duration(500)
+              .attr("x", function(d) { return x(d[scope.scalex]); })
+              .attr("y", function(d) {
+                return y(d.y1); })
+              .attr("height", function(d) { return (y(d.y0) - y(d.y1)); })
+              .style("opacity", 0.9);
 
-          // bars.transition()
-          //   .duration(500)
-          //   .attr("class", "bar")
-          //   .attr("x", function(d) { return x(d[scope.scalex]); })
-          //   .attr("y", function(d) { return y(d[scope.scaley]); })
-          //   .attr("height", function(d) { return height - y(d[scope.scaley]); })
-          //   .attr("width", x.rangeBand())
-          //   .attr("fill", function(d) {
-          //     return teamColors[d.team].fill;
-          //   })
-          //   .attr("stroke", function(d) {
-          //     return teamColors[d.team].stroke;
-          //   });
-
-          //enter
-          // bars.enter()
-          //   .append("rect")
-          //   .attr("width", x.rangeBand())
-          //   .attr("class", "bar")
-          //   .attr("x", 0)
-          //   .attr("y", y(0))
-          //   .attr("height", 0)
-          //   .style("opacity", 0)
-          //   .on('mouseover', function(d,i) {
-          //     scope.$apply(scope.hover({item: d}));
-          //     //return scope.hover({item: d});
-          //   })
-          //   .on('mouseleave', function(d,i) {
-          //     scope.$apply(scope.hoverLeave({item: d}));
-          //     //return scope.hoverLeave({item: d});
-          //   })
-          //   .attr("fill", function(d) {
-          //     return teamColors[d.team].fill;
-          //   })
-          //   .attr("stroke", function(d) {
-          //     return teamColors[d.team].stroke;
-          //   })
-          //   .transition()
-          //     .duration(500)
-          //     .attr("y", function(d) { return y(d[scope.scaley]); })
-          //     .attr("x", function(d) { return x(d[scope.scalex]); })
-          //     .attr("height", function(d) { return height - y(d[scope.scaley]); })
-          //     .style("opacity", 0.8);
-
-          //   bars.exit()
-          //     .transition()
-          //     .duration(500)
-          //     .style("opacity", 0)
-          //     .remove();
+            bars.exit()
+              .transition()
+              .duration(500)
+              .style("opacity", 0)
+              .remove();
         }
 
         update(data);
+
         scope.cityName = teamBackgrounds.all;
         scope.team = function(team) {
           if (team === "all") {
@@ -255,7 +260,7 @@ nwslData
           }
         };
 
-        scope.sortText = "Sort by Total Goals";
+        var orig = scope.sortText;
 
         var sortOrder = true;
         scope.sort = function() {
@@ -293,7 +298,7 @@ nwslData
             .selectAll("g")
             .delay(delay);
           } else {
-            scope.sortText = "Sort by Total Goals";
+            scope.sortText = orig;
             var x0 = x.domain(data.sort(function(a,b) {
               if (a.team === b.team) {
                 if (a[scope.scaley] > b[scope.scaley]) return -1;
@@ -326,7 +331,7 @@ nwslData
             .delay(delay);
           }
         };
-       });
+      });
     }
   };
 });
