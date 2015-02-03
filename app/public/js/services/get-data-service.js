4493d08838a60bcbd1519c78943abab79b342259
Hanna Liebl <hanna@hannaliebl.com>
2015-02-03 09:31:08 -0800

    add service to simply get data given a year

---
+++ app/public/js/services/get-data-service.js
@@ -0,0 +1,19 @@
+nwslData
+.factory('getDataService', function ($http, $q) {
+  'use strict';
+
+  return {
+    getRawData: function(year) {
+
+      var deferred = $q.defer();
+
+      $http.get('/api/' + year + '?callback=JSON_CALLBACK').success(function (result) {
+        deferred.resolve(result);
+        }).error(function () {
+          deferred.reject('Could not load raw NWSL data.');
+        });
+
+      return deferred.promise;
+    }
+  };
+});
