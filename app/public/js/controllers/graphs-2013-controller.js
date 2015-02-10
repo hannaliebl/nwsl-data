f129f0b7581160ee966a2a2159f6fdfadc812ad4
Hanna Liebl <hanna.liebl@idexpertscorp.com>
2015-02-09 22:35:04 -0800

    rename scope variables with their year

--- app/public/js/controllers/graphs-2013-controller.js
+++ app/public/js/controllers/graphs-2013-controller.js
@@ -2,29 +2,31 @@ nwslData
 .controller('Graphs2013Ctrl', function ($scope, getDataService, nwslDataService, graphDataService) {
   'use strict';
 
-  $scope.rawData = [];
-  $scope.goalScorers = [];
-  $scope.cities = [];
-  $scope.goalsPerHr = [];
-  $scope.goalsAllowedPerGame = [];
-  $scope.saves = [];
-  $scope.goalsAllowed = [];
-  $scope.totalFouls = [];
-  $scope.yellowCards = [];
+  $scope.rawData2013 = [];
+  $scope.goalScorers2013 = [];
+  $scope.offFrameShots2013 = [];
+  $scope.cities2013 = [];
+  $scope.goalsPerHr2013 = [];
+  $scope.goalsAllowedPerGame2013 = [];
+  $scope.saves2013 = [];
+  $scope.goalsAllowed2013 = [];
+  $scope.totalFouls2013 = [];
+  $scope.yellowCards2013 = [];
   $scope.loading = true;
 
   $scope.graphData = graphDataService.data;
 
   $scope.$watch('graphData', function(newVal) {
-    $scope.rawData = newVal.rawData;
-    $scope.goalScorers = newVal.goalScorers;
-    $scope.cities = newVal.teams;
-    $scope.goalsPerHr = newVal.goalsPerHr;
-    $scope.goalsAllowedPerGame = newVal.goalsAllowedPerGame;
-    $scope.saves = newVal.saves;
-    $scope.goalsAllowed = newVal.goalsAllowed;
-    $scope.totalFouls = newVal.totalFouls;
-    $scope.yellowCards = newVal.yellowCards;
+    $scope.rawData2013 = newVal.rawData;
+    $scope.goalScorers2013 = newVal.goalScorers;
+    $scope.offFrameShots2013 = newVal.offFrameShots;
+    $scope.cities2013 = newVal.teams;
+    $scope.goalsPerHr2013 = newVal.goalsPerHr;
+    $scope.goalsAllowedPerGame2013 = newVal.goalsAllowedPerGame;
+    $scope.saves2013 = newVal.saves;
+    $scope.goalsAllowed2013 = newVal.goalsAllowed;
+    $scope.totalFouls2013 = newVal.totalFouls;
+    $scope.yellowCards2013 = newVal.yellowCards;
     $scope.loading = newVal.loading;
   }, true);
 
